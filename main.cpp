aac2b9120a3d39a696dcbf1ad30518c9a86974a7
FearlessAtom <atomgotaggressive@gmail.com>
2025-05-14 14:20:43 +0300

    adding an external library

--- main.cpp
+++ main.cpp
@@ -12,12 +12,13 @@ int get_random_integer(int min, int max)
 }
 
 typedef ICalculator* (*create_calculator_function)();
+typedef void (*hello_function)();
 
 int main()
 {
     std::string calculator_library_path= "./calculator/calculator.dll";
 
-    int length = 5, min = -10, max = 10;
+    int length = 10, min = -10, max = 10;
     int* array = new int[length];
 
     for (int i = 0; i < length; i++) array[i] = get_random_integer(min, max);
@@ -29,7 +30,7 @@ int main()
 
     if (!module)
     {
-        std::cerr << std::endl << "Failed to load the calculator module!" << std::endl;
+        std::cerr << "Loading calculator library failed with error code: " << GetLastError() << std::endl;
         return 1;
     }
 
@@ -51,6 +52,18 @@ int main()
     FreeLibrary(module);
 
     for (int i = 0; i < length; i++) std::cout << array[i] << " ";
+
+    HMODULE hello_module = LoadLibrary("./hello_world.dll");
+
+    if (!hello_module)
+    {
+        std::cerr << std::endl << "Loading hello library failed with error code: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    hello_function hello = (hello_function)GetProcAddress(hello_module, NULL);
+    
+    hello();
 
     return 0;
 }
